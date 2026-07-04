ff39cd821b22236151a5a04fd22b6c399b111081
user1 <user1@NIBMADMIN03s-iMac.local>
2026-07-04 17:29:27 +0530

    check 1

--- Ios Development/ContentView.swift
+++ Ios Development/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("chicken fried rice")
         }
         .padding()
     }
