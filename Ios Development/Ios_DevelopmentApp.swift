00744acebb669bc198617c004cecd42016d7afc7
user1 <user1@NIBMADMIN03s-iMac.local>
2026-07-04 17:25:15 +0530

    initial commit

---
+++ Ios Development/Ios_DevelopmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ios_DevelopmentApp.swift
+//  Ios Development
+//
+//  Created by user1 on 2026-07-04.
+//
+
+import SwiftUI
+
+@main
+struct Ios_DevelopmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
